a732287fc56da224ee8a811f254efd8d4e07f377
Enrique Corpa Rios <38493088+2nOrderEDO@users.noreply.github.com>
2019-04-04 09:39:23 +0200

    Micro refactor

--- format_error.py
+++ format_error.py
@@ -3,8 +3,7 @@ import numpy as np
 def format_error(value, error):
     # Rounding method used is banker's rounding
     
-    error = np.format_float_scientific(error, 0) # print error as sci. notation rounding
-    digits, exp = error.split('e') # extract digits and exponent
+    digits, exp = np.format_float_scientific(error, 0).split('e') # print as sci. notation, extract digits and exponent
     decimals = abs(int(exp)) if int(exp) <= 0 else 0
     digits = np.around(float(digits))
     value = np.around(value, -int(exp)) # adjust significant digits to the ones allowed by the error.
